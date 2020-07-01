d561c0df5f02a2a325eabf3080eeee9e6821f9e6
Keith Alcock <github@keithalcock.com>
2020-06-30 22:39:26 -0700

    Make example

--- examples/ser-par/test_ser-par.cc
+++ examples/ser-par/test_ser-par.cc
@@ -1,14 +1,17 @@
 #include <dynet/dynet.h>
-#include <dynet/expr.h>
-#include <dynet/exec.h>
 #include <dynet/lstm.h>
-#include <dynet/fast-lstm.h>
-#include <dynet/gru.h>
-#include <dynet/grad-check.h>
-#include <dynet/param-init.h>
 
 #include <thread>
 
+/*
+It works without dynamic memory if thread count is 1.
+It does not work without dynamic memory if thread count is 2.
+  An exception is purposely thrown from the C++ code.
+
+It works with dynamic memory if thread count is 1.
+It does not work with dynamic memory if thread count is 2.
+  An exception is thrown by the OS during operator delete.
+*/
 using namespace dynet;
 using namespace std;
 
@@ -18,19 +21,22 @@ int main(int _argc, char** _argv) {
   char* args[] = {
     "--dynet-seed", "10",
     "--dynet-mem", "1",
-    "--dynet-dynamic-mem", "0"
+    "--dynet-dynamic-mem", "1"
   };
   char** argv = &args[0];
   int argc = 6;
 
   dynet::initialize(argc, argv);
 
-  const int threadCount = 1;
+  const int threadCount = 2;
+  const int layers = 2;
+  const unsigned int inputDim = 3;
+  const unsigned int hiddenDim = 10;
 
   vector<vector<float>> results(threadCount);
   dynet::ParameterCollection model;
-  dynet::VanillaLSTMBuilder lstm_proto(2, 3, 10, model);
-  dynet::LookupParameter lp_proto = model.add_lookup_parameters(10, {3});
+  dynet::VanillaLSTMBuilder protoLstmBuilder(layers, inputDim, hiddenDim, model);
+  dynet::LookupParameter protoLookupParameter = model.add_lookup_parameters(hiddenDim, {inputDim});
   dynet::autobatch_flag = 0;
   
   vector<thread> threads(threadCount);
@@ -38,32 +44,30 @@ int main(int _argc, char** _argv) {
     threads[t] = thread([&, t]() {
       cout << "Thread started!" << endl;
       dynet::ComputationGraph cg;
-      dynet::VanillaLSTMBuilder lstm(lstm_proto);
-      dynet::LookupParameter lp(lp_proto);
-      lstm.new_graph(cg);
+      dynet::VanillaLSTMBuilder lstmBuilder(protoLstmBuilder);
+      dynet::LookupParameter lookupParameter(protoLookupParameter);
+      lstmBuilder.new_graph(cg);
         
       vector<Expression> losses;
-      for (size_t j = 0; j < 3; ++j) {
-        lstm.start_new_sequence();
-        for (size_t k = 0; k < 3; ++k) {
-          Expression x = dynet::lookup(cg, lp, j*3 + k);
-          lstm.add_input(x);
+      for (size_t j = 0; j < inputDim; ++j) {
+        lstmBuilder.start_new_sequence();
+        for (size_t k = 0; k < inputDim; ++k) {
+          Expression x = dynet::lookup(cg, lookupParameter, j * inputDim + k);
+          lstmBuilder.add_input(x);
         }
-        losses.push_back(squared_norm(lstm.final_h()[1]));
+        losses.push_back(squared_norm(lstmBuilder.final_h()[layers - 1]));
       }
-      losses.push_back(losses[0] + losses[2]);
+      losses.push_back(losses[0] + losses[inputDim - 1]);
       Expression z = dynet::sum(losses);
       results[t].push_back(as_scalar(z.value()));
       cout << "Thread finished!" << endl;
     });
   }
 
-  for (size_t t = 0; t < threadCount; ++t) { threads[t].join(); }
-  for (size_t t = 0; t < threadCount; ++t) {
-    for (size_t i = 1; i < results[t].size(); ++i) {
-    if (abs(results[t][0] - results[t][i]) >= 0.0001)
-      cerr << "parallel test failed" << endl;
-    }
-  }
+  for (size_t t = 0; t < threadCount; ++t) threads[t].join();
+  for (size_t t = 0; t < threadCount; ++t)
+    for (size_t i = 1; i < results[t].size(); ++i)
+      if (abs(results[t][0] - results[t][i]) >= 0.0001)
+        cerr << "Parallel test failed!" << endl;
   cout << "Program finished!" << endl;
 }
