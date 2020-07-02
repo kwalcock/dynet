98e5b06e02d948a6fcb9646b7f597bcdd84d7136
Keith Alcock <github@keithalcock.com>
2020-07-02 16:55:34 -0700

    Add an additional test program

---
+++ examples/overlap/test_overlap.cc
@@ -0,0 +1,88 @@
+#include <dynet/dynet.h>
+#include <dynet/lstm.h>
+
+#include <thread>
+
+/*
+It works without dynamic memory if thread count is 1.
+It does not work without dynamic memory if thread count is 2.
+  An exception is purposely thrown from the C++ code.
+
+It works with dynamic memory if thread count is 1.
+It does not work with dynamic memory if thread count is 2.
+  An exception is thrown by the OS during operator delete.
+*/
+
+class OverlappingLstm {
+
+public:
+  dynet::ComputationGraph cg;
+  dynet::VanillaLSTMBuilder lstmBuilder;
+  dynet::LookupParameter lookupParameter;
+  int t;
+
+  OverlappingLstm(int t, dynet::VanillaLSTMBuilder &protoLstmBuilder, dynet::LookupParameter &protoLookupParameter):
+      t(t), lstmBuilder(protoLstmBuilder), lookupParameter(protoLookupParameter) {
+    std::cout << "Thread " << t << " started!" << std::endl;
+    lstmBuilder.new_graph(cg);
+
+  }
+
+  std::vector<float> test(int layers, int inputDim) {
+    std::vector<dynet::Expression> losses;
+    for (size_t j = 0; j < inputDim; ++j) {
+      lstmBuilder.start_new_sequence();
+      for (size_t k = 0; k < inputDim; ++k) {
+        dynet::Expression x = dynet::lookup(cg, lookupParameter, j * inputDim + k);
+        lstmBuilder.add_input(x);
+      }
+      losses.push_back(dynet::squared_norm(lstmBuilder.final_h()[layers - 1]));
+    }
+    losses.push_back(losses[0] + losses[inputDim - 1]);
+    dynet::Expression z = dynet::sum(losses);
+    std::vector<float> results;
+    results.push_back(dynet::as_scalar(z.value()));
+    std::cout << "Thread " << t << " finished!" << std::endl;
+    return results;
+  }
+};
+
+
+int main(int _argc, char** _argv) {
+  const int threadCount = 1;
+
+  std::cout << "Program started for " << threadCount << " threads!" << std::endl;
+
+  char* args[] = {
+    "",
+    "--dynet-seed", "10",
+    "--dynet-mem", "1",
+    "--dynet-dynamic-mem", "1"
+  };
+  char** argv = &args[0];
+  int argc = 7;
+
+  dynet::initialize(argc, argv);
+
+  const int layers = 2;
+  const unsigned int inputDim = 3;
+  const unsigned int hiddenDim = 10;
+
+  std::vector<std::vector<float>> results(threadCount);
+  dynet::ParameterCollection model;
+  dynet::VanillaLSTMBuilder protoLstmBuilder(layers, inputDim, hiddenDim, model);
+  dynet::LookupParameter protoLookupParameter = model.add_lookup_parameters(hiddenDim, { inputDim });
+  dynet::autobatch_flag = 0;
+
+  // This should work OK if they are nested, but not if they are not.
+  OverlappingLstm overlappingLstm1(1, protoLstmBuilder, protoLookupParameter);
+//  OverlappingLstm overlappingLstm2(2, protoLstmBuilder, protoLookupParameter);
+  results[0] = overlappingLstm1.test(layers, hiddenDim);
+//  results[1] = overlappingLstm2.test(layers, hiddenDim);
+
+  for (size_t t = 0; t < threadCount; ++t)
+    for (size_t i = 1; i < results[t].size(); ++i)
+      if (abs(results[t][0] - results[t][i]) >= 0.0001)
+        std::cerr << "Parallel test failed!" << std::endl;
+  std::cout << "Program finished!" << std::endl;
+}
