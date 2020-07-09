bf7492b98bf355f9337cc165e9a5139d2debdec3
Keith Alcock <github@keithalcock.com>
2020-07-09 16:59:07 -0700

    Add C++

--- dynet/mem_debug.h
+++ dynet/mem_debug.h
@@ -1,6 +1,8 @@
 #ifndef DYNET_MEM_DEBUG_H
 #define DYNET_MEM_DEBUG_H
 
+// See https://docs.microsoft.com/en-us/visualstudio/debugger/finding-memory-leaks-using-the-crt-library?view=vs-2019 .
+
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
@@ -14,6 +16,14 @@ public:
   MemDebug();
   ~MemDebug();
 };
+
+// Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
+// allocations to be of _CLIENT_BLOCK type
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
+
+#else
+
+#define DBG_NEW new
 
 #endif
 
