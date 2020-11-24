6e189859b1cea958cdfef746ced862508937d336
Keith Alcock <github@keithalcock.com>
2020-11-24 12:15:29 -0700

    Split into two parts

--- dynet/signalhandler.cc
+++ dynet/signalhandler.cc
@@ -2,76 +2,63 @@
 
 #include <csignal>
 #include <iostream>
-#include <map>
-#include <memory>
 
 namespace dynet {
 
-  class SignalHandler {
-   public:
-    SignalHandler();
-    virtual ~SignalHandler();
-    virtual int run(int signal);
-  };
+SignalHandler::SignalHandler() { }
 
-  SignalHandler::SignalHandler() { }
+SignalHandler::~SignalHandler() { }
 
-  SignalHandler::~SignalHandler() { }
+int SignalHandler::run(int signal) {
+  std::cout << "Callback::run()" << std::endl;
+  return 0;
+}
 
-  int SignalHandler::run(int signal) {
-    std::cout << "Callback::run()" << std::endl;
-    return 0;
+void runSignalHandler(int signal);
+
+void SignalHandlerHolder::set(int signal, SignalHandler* signalHandler) {
+  del(signal);
+  signalHandlers[signal] = signalHandler;
+  std::signal(signal, runSignalHandler);
+}
+
+void SignalHandlerHolder::run(int signal) {
+  signalHandlers[signal]->run(signal);
+}
+
+void SignalHandlerHolder::reset(int signal) {
+  del(signal);
+}
+
+SignalHandlerHolder::~SignalHandlerHolder() {
+  for (const auto& keyValue : signalHandlers) {
+    std::signal(keyValue.first, SIG_DFL);
+    delete keyValue.second;
   }
+  signalHandlers.clear();
+}
 
-  void runSignalHandler(int signal);
-
-  class SignalHandlerHolder {
-   protected:
-    std::map<int, SignalHandler*> signalHandlers;
-   public:
-    void set(int signal, SignalHandler* signalHandler) {
-      del(signal);
-      signalHandlers[signal] = signalHandler;
-      std::signal(signal, runSignalHandler);
-    }
-
-    void run(int signal) {
-      signalHandlers[signal]->run(signal);
-    }
-
-    void reset(int signal) {
-      del(signal);
-    }
-
-    ~SignalHandlerHolder() {
-      for (const auto& keyValue : signalHandlers) {
-        std::signal(keyValue.first, SIG_DFL);
-        delete keyValue.second;
-      }
-      signalHandlers.clear();
-    }
-   protected:
-    void del(int signal) {
-      std::signal(signal, SIG_DFL);
-      std::map<int, SignalHandler*>::iterator signalHandlerItr = signalHandlers.find(signal);
-      if (signalHandlerItr != signalHandlers.end()) {
-        signalHandlers.erase(signalHandlerItr);
-        delete signalHandlerItr->second;
-      }
-    }
-  };
-
-  SignalHandlerHolder signalHandlerHolder;
-
-  void setSignalHandler(int signal, SignalHandler* signalHandler) {
-    signalHandlerHolder.set(signal, signalHandler);
+void SignalHandlerHolder::del(int signal) {
+  std::signal(signal, SIG_DFL);
+  std::map<int, SignalHandler*>::iterator signalHandlerItr = signalHandlers.find(signal);
+  if (signalHandlerItr != signalHandlers.end()) {
+    signalHandlers.erase(signalHandlerItr);
+    delete signalHandlerItr->second;
   }
+}
 
-  void resetSignalHandler(int signal) {
-    signalHandlerHolder.reset(signal);
-  }
+SignalHandlerHolder signalHandlerHolder;
 
-  static void runSignalHandler(int signal) {
-    signalHandlerHolder.run(signal);
-  }
+void setSignalHandler(int signal, SignalHandler* signalHandler) {
+  signalHandlerHolder.set(signal, signalHandler);
+}
+
+void resetSignalHandler(int signal) {
+  signalHandlerHolder.reset(signal);
+}
+
+static void runSignalHandler(int signal) {
+  signalHandlerHolder.run(signal);
+}
+
 }
--- dynet/signalhandler.h
+++ dynet/signalhandler.h
@@ -1,7 +1,31 @@
 #ifndef DYNET_SIGNALHANDLER_H
 #define DYNET_SIGNALHANDLER_H
 
+#include <map>
+
 namespace dynet {
+
+class SignalHandler {
+ public:
+  SignalHandler();
+  virtual ~SignalHandler();
+  virtual int run(int signal);
+};
+
+class SignalHandlerHolder {
+ protected:
+  std::map<int, SignalHandler*> signalHandlers;
+ public:
+  void set(int signal, SignalHandler* signalHandler);
+  void run(int signal);
+  void reset(int signal);
+  ~SignalHandlerHolder();
+ protected:
+  void del(int signal);
+};
+
+void setSignalHandler(int signal, SignalHandler* signalHandler);
+void resetSignalHandler(int signal);
 
 } // namespace dynet
 
